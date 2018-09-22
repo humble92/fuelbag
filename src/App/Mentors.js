931745550cb1dd71b2f101cce1e40a7e018ce05a
Timofey Smirnov <tim@driveback.ru>
2018-09-22 15:50:29 -0700

    added mentors

--- src/App/Mentors.js
+++ src/App/Mentors.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import mentors from '../data/mentors'
 
 export default () => (
-  <div>
-    Mentors list
+  <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+  }}>
+    <div style={{
+      width: 500,
+      backgroundColor: 'white',
+      margin: 20,
+    }}>
+      {mentors.map(mentor =>
+        <div style={{
+          fontSize: 18,
+          border: '1px solid black',
+          padding: 10,
+          display: 'flex',
+        }}>
+          <div style={{
+            minWidth: 100,
+            minHeight: 100,
+            marginRight: 10,
+            backgroundColor: 'gray',
+          }} />
+          <div>
+            <div><h3>{mentor.firstName} {mentor.lastName}</h3></div>
+            <div>{mentor.role}</div>
+            <div>{mentor.company}</div>
+          </div>
+        </div>
+      )}
+    </div>
   </div>
 )

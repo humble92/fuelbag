8f5fa40344b547f58d7ef749c0ec2d475480e6dd
Timofey Smirnov <tim@driveback.ru>
2018-09-22 13:19:33 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <header>
+      <h1>Welcome to Fuelbag</h1>
+    </header>
+    <p>
+      Let's break the cycle of poverty together!
+    </p>
+  </div>
+)
+
+export default App

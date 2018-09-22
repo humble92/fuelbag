fe2abe413d4c44f5c3f3c11bd1b8a07b8e9acde3
Timofey Smirnov <tim@driveback.ru>
2018-09-22 13:36:00 -0700

    added redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App/App'
+import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import createStore from './redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const renderApp = (store) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  )
+}
+
+const loadApp = async () => {
+  const store = await createStore()
+  renderApp(store)
+
+  if (module.hot) {
+    module.hot.accept(
+      './App/App',
+      () => renderApp(store),
+    )
+  }
+}
+
+loadApp()
+registerServiceWorker()
---
+++ src/redux/index.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducer from './reducer'
+
+export default async () => {
+  const state = {}
+  const store = createStore(
+    reducer,
+    state,
+    composeWithDevTools(applyMiddleware(thunk)),
+  )
+
+  if (module.hot) {
+    module.hot.accept('./reducer', () => {
+      store.replaceReducer(require('./reducer'))
+    })
+  }
+
+  return store
+}
